bc5bd1c520063e0acbca08b3b189e4650523f35f
DESKTOP-6K6QA88\ashlyn <ftyashlyn@gmail.com>
2017-12-15 21:52:31 +0800

    Modify the previous code

--- zerver/lib/actions.py
+++ zerver/lib/actions.py
@@ -1634,7 +1634,8 @@ def check_send_message(sender, client, message_type_name, message_to,
                        sender_queue_id=None):
     # type: (UserProfile, Client, Text, Sequence[Text], Optional[Text], Text, Optional[Realm], bool, Optional[float], Optional[UserProfile], Optional[Text], Optional[Text]) -> int
     hahaha = print(message_content)
-    print(hahaha)
+    if print(hahaha) = 'welcome':
+        message_content = 'Welcome to Zulip :octopus:'
     addressee = Addressee.legacy_build(
         sender,
         message_type_name,
